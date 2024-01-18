98cfd44eed417af8b97070917b95cbb9715c2b5c
Juliasse <vinicius.dantas@raizen.com>
2024-01-18 10:41:25 -0300

    Criando um pull request template

--- retorna_conteudo_pr_template.py
+++ retorna_conteudo_pr_template.py
@@ -2,70 +2,67 @@ import base64
 import sys
 
 def main(name, owner, project, area):
-    content = f'''name: Bug Report
-description: File a bug report
-title: "[Bug]: "
-labels: ["bug", "triage"]
-projects: ["octo-org/1", "octo-org/44"]
-assignees:
-  - octocat
-body:
-  - type: markdown
-    attributes:
-      value: |
-        Thanks for taking the time to fill out this bug report!
-  - type: input
-    id: contact
-    attributes:
-      label: Contact Details
-      description: How can we get in touch with you if we need more info?
-      placeholder: ex. email@example.com
-    validations:
-      required: false
-  - type: textarea
-    id: what-happened
-    attributes:
-      label: What happened?
-      description: Also tell us, what did you expect to happen?
-      placeholder: Tell us what you see!
-      value: "A bug happened!"
-    validations:
-      required: true
-  - type: dropdown
-    id: version
-    attributes:
-      label: Version
-      description: What version of our software are you running?
-      options:
-        - 1.0.2 (Default)
-        - 1.0.3 (Edge)
-      default: 0
-    validations:
-      required: true
-  - type: dropdown
-    id: browsers
-    attributes:
-      label: What browsers are you seeing the problem on?
-      multiple: true
-      options:
-        - Firefox
-        - Chrome
-        - Safari
-        - Microsoft Edge
-  - type: textarea
-    id: logs
-    attributes:
-      label: Relevant log output
-      description: Please copy and paste any relevant log output. This will be automatically formatted into code, so no need for backticks.
-      render: shell
-  - type: checkboxes
-    id: terms
-    attributes:
-      label: Code of Conduct
-      description: By submitting this issue, you agree to follow our [Code of Conduct](https://example.com)
-      options:
-        - label: I agree to follow this project's Code of Conduct
-          required: true'''
+    content = f'''---
+name: Modelo Pull Request
+about: Describe this issue template's purpose here.
+title: Modelo Pull Request
+labels: ''
+assignees: claricepdantas
+
+---
+
+# Pull Request
+
+## Descrição
+
+Descreva brevemente as alterações implementadas nesta solicitação de pull.
+
+## Problema
+
+Explique o problema ou a necessidade que essa solicitação de pull visa resolver.
+
+## Solução
+
+Descreva a solução implementada e como ela aborda o problema mencionado acima.
+
+## Alterações Propostas
+
+Liste as principais alterações realizadas no código.
+
+- [ ] Alteração 1
+- [ ] Alteração 2
+- [ ] ...
+
+## Testes Realizados
+
+Descreva os testes que foram realizados para garantir que as alterações não introduziram novos problemas.
+
+- [ ] Teste A
+- [ ] Teste B
+- [ ] ...
+
+## Capturas de Tela (se aplicável)
+
+Inclua capturas de tela relevantes, se necessário.
+
+## Checklist
+
+- [ ] Minhas alterações passaram nos testes locais.
+- [ ] Eu atualizei a documentação conforme necessário.
+- [ ] Os testes de unidade foram adicionados ou atualizados conforme necessário.
+- [ ] Eu li o guia de contribuição e segui as diretrizes.
+
+## Comentários Adicionais
+
+Forneça qualquer informação adicional que você julgar relevante.
+
+## Issue Relacionada
+
+Indique se há uma issue relacionada a este pull request.
+
+## Pontos de Atenção
+
+Destaque áreas específicas que podem precisar de revisão ou atenção extra.'''
     return base64.b64encode(str(content).encode()).decode()
 
 
