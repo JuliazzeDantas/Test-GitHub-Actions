c9d765236fe72bfc1d3b86e72b59f569140e805b
Juliasse <vinicius.dantas@raizen.com>
2024-01-17 21:49:33 -0300

    Inserindo conteúdo no readme2

--- retorna_conteudo_readme.py
+++ retorna_conteudo_readme.py
@@ -2,12 +2,13 @@ import base64
 import sys
 
 def main(name, owner, project, area):
-    content = f'''# Repositório {name}\n\n
-    ## Owner: {owner}\n\n
-    ## Project: {project}\n\n ## Área: {area}'''
+    content = f'''# Repositório {name}\n
+## Owner: {owner}\n
+## Project: {project}\n
+## Área: {area}'''
     return base64.b64encode(str(content).encode()).decode()
 
 
 
 if __name__ == "__main__":
-    print(main(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[3]))
+    print(main(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4]))
